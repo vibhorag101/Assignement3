06478035d4947c35f07e2174ea62da1d005001fb
Vibhor Agarwal <vibhor20349@iiitd.ac.in>
2021-04-12 10:25:17 +0530

    corrected circel menu with init cordinates

--- a3.py
+++ a3.py
@@ -471,16 +471,21 @@ if __name__ == "__main__":
             querylist=input("enter query\n").split()
             if querylist[0]=="T":
                 if len(querylist)==2:
+                    print(*[menu.xcord,menu.ycord,menu.circlerad])
                     print(*menu.translate(float(querylist[1])))
                 elif len(querylist)==3:
+                    print(*[menu.xcord,menu.ycord,menu.circlerad])
                     print(*menu.translate(float(querylist[1]),float(querylist[2])))
             
             elif querylist[0]=="R":
                 if len(querylist)==2:
+                    print(*[menu.xcord,menu.ycord,menu.circlerad])
                     print(*menu.rotate(float(querylist[1])))
                 elif len(querylist)==3:
+                    print(*[menu.xcord,menu.ycord,menu.circlerad])
                     print(*menu.rotate(float(querylist[1]),float(querylist[2])))
             elif querylist[0]=="S":
+                print(*[menu.xcord,menu.ycord,menu.circlerad])
                 print(*menu.scale(float(querylist[1])))
             elif querylist[0]=="P":
                 menu.plot()
